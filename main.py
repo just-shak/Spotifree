ca92f8cbdbabd278cba575862ca779b04b9e1880
just-shak <justineleclercq@gmail.com>
2022-06-16 16:46:38 +0200

    check user in bdd + redirection register

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from crypt import methods
 import os
 from flask import Flask, render_template, request, redirect, session
+from bdd import Bdd
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12).hex()
+spotifree_bdd = Bdd()
 data = []
 
 
@@ -11,14 +12,39 @@ data = []
 def home():
     if "user" in session:
         return render_template("accueil.html", datas=data, name=session["user"])
+
     return render_template("accueil.html")
 
 
 @app.route("/login", methods=["POST"])
 def login():
-    session["user"] = request.form["user"]
-    data.append(request.form["user"])
-    return redirect("/")
+    session["email"] = request.form["email"]
+    if request.form["email"] in spotifree_bdd.read_one(
+        column_read="email",
+        table_name="users",
+        column_match="email",
+        text=request.form["email"],
+    ):
+        # data.append(request.form["email"])
+        return redirect("/music_search")
+    else:
+        return redirect("/register")
+
+
+# @app.route("/login", methods=["POST"])
+# def login():
+#     session["user"] = request.form["user"]
+#     data.append(request.form["user"])
+#     # if request.form["email"] in spotifree_bdd.read_one(
+#     #     column_read="email",
+#     #     table_name="users",
+#     #     column_match="email",
+#     #     text=request.form["email"],
+#     # ):
+#     #     return redirect("/music_search")
+#     # else:
+#     #     return redirect("/register2")
+#     return redirect("/")
 
 
 @app.route("/disconnect")
