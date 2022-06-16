b9ee4e6da4ced9819485c15a3edd8531c497c8b7
just-shak <justineleclercq@gmail.com>
2022-06-16 14:48:08 +0200

    add music_search.html into main.py

--- main.py
+++ main.py
@@ -4,25 +4,33 @@ from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12).hex()
+data = []
 
 
 @app.route("/")
 def home():
     if "user" in session:
-        return render_template("accueil.html", name=session["user"])
+        return render_template("accueil.html", datas=data, name=session["user"])
     return render_template("accueil.html")
 
 
 @app.route("/login", methods=["POST"])
 def login():
     session["user"] = request.form["user"]
+    data.append(request.form["user"])
     return redirect("/")
 
 
 @app.route("/disconnect")
 def disconnect():
     session.pop("user")
+    data.clear()
     return redirect("/")
+
+
+@app.route("/music_search")
+def music_search():
+    return render_template("search.html")
 
 
 if __name__ == "__main__":
